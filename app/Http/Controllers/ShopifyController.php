15f03250aa552fcd9e9ceb91117d364bf97cfe56
Edgar Sidawi <romantibuai@gmail.com>
2019-12-25 19:58:38 +0000

    app cleanup

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -17,13 +17,6 @@ class ShopifyController extends Controller
 
     public function getProducts(Request $request)
     {
-        // $products = $this->shopify->setShopUrl("my-laravel-store.myshopify.com")
-        //     ->setAccessToken("18a4af1b20f3dcafe25b9db4ab57c90e")
-        //     ->get('admin/products.json');
-
-        // $shopUrl = "my-laravel-store.myshopify.com";
-        // $accessToken = "18a4af1b20f3dcafe25b9db4ab57c90e";
-
         $products = Shopify::setShopUrl($this->shopUrl)->setAccessToken($this->accessToken)->get("admin/products.json");
 
         return response($products, 200);
@@ -39,12 +32,6 @@ class ShopifyController extends Controller
             "admin/products.json",
             [
                 "product" => $request->all()
-                // $request->all()
-
-                // "title" => "Nike football boots",
-                // "body_html" => "A very well made football boot by Nike",
-                // "vendor" => "Nike",
-                // "product_type" => "football boot"
             ]
         );
 
@@ -68,7 +55,6 @@ class ShopifyController extends Controller
     public function deleteProduct(Request $request)
     {
         // return ($request->id);
-
         $products = Shopify::setShopUrl($this->shopUrl)->setAccessToken($this->accessToken)->delete(
             "admin/products/{$request->id}.json"
         );
